6d9a54f7afece6d09c0c4a55ba21a5641d10601e
Erik B <ebz.erik@gmail.com>
2024-05-20 16:59:29 -0600

    fix Ventilador.py 3

--- utils/Ventilador.py
+++ utils/Ventilador.py
@@ -1,14 +1,14 @@
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-MOTOR_PIN = 18
+MOTOR_PIN = 27
 GPIO.setup(MOTOR_PIN, GPIO.OUT)
 
 
 class Fan:
     def __init__(self):
         self.pwm = GPIO.PWM(MOTOR_PIN, 100)  # Frecuencia de 100 Hz
-        self.pwm.start(0)
+        self.pwm.start(100)
 
     def set_speed(self, speed: int):
         if speed < 0 or speed > 100:
