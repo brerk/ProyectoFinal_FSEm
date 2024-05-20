dafd64756e55a779a2d9afe2c9ad3d825c216dc0
Erik B <ebz.erik@gmail.com>
2024-05-20 16:13:24 -0600

    install.sh fix & led_control file

--- install.sh
+++ install.sh
@@ -1,9 +1,59 @@
 #!/bin/bash
 
-apt-get install virtualenv  -y
+if [ "$EUID" -ne 0 ]
+then echo "Script must be run as root. (try sudo su?)"
+  exit
+fi
 
-# TODO: Enable 1-wire
+echo "Enable 1-Wire"
+echo "dtoverlay=w1-gpio" >> /boot/firmware/config.txt
+
+echo "Eable I2C mod"
+raspi-config nonint do_i2c 0
+
+echo "Installing system dependencies"
+apt-get install i2c-tools python3-smbus2 python3-pip virtualenv ufw git -y
+adduser pi i2c
+
+modprobe w1-gpio
+modprobe w1-therm
+
+echo "Connected 1-wire devices."
+ls sys/bus/w1/devices
+
+ufw allow 8000 comment "invernadero web interface"
+
+echo "Proyect Installation..."
+
+
+cd /home/pi
 git clone https://github.com/brerk/ProyectoFinal_FSEm.git
+chown -R pi:pi /home/pi/ProyectoFinal_FSEm
 cd ProyectoFinal_FSEm
 
+echo "Creating Virtual environment..."
 virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+echo "Installing proyect as service..."
+
+cat << EOF > /etc/systemd/system/invernadero.service
+[Unit]
+Description=Invernadero Inteligente 
+After=network.target
+
+[Service]
+Type=simple
+User=pi
+WorkingDirectory=/home/pi/ProyectoFinal_FSEm
+Environment=PYTHONPATH=/home/pi/ProyectoFinal_FSEm/venv
+ExecStart=/home/pi/ProyectoFinal_FSEm/venv/bin/fastapi run server.py
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable --now inveradero.service
