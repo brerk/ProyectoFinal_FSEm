47c900243b8e2fa6ffaa290a89b9fb0847ef78d6
Erik B <ebz.erik@gmail.com>
2024-05-27 14:13:33 -0600

    fix disable sensors

--- server.py
+++ server.py
@@ -255,7 +255,7 @@ def get_current_temp() -> float:
     s0_temp = i2c_handler.read_temp_from_i2c(0)
     s1_temp = i2c_handler.read_temp_from_i2c(1)
 
-    temp_prom = (s0_temp + s1_temp)/2
+    # temp_prom = (s0_temp + s1_temp)/2
 
     # return temp_prom
     return None
